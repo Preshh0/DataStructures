37a34da74d34b349f4c8368e60a4794b07f43c82
Preshh0 <uniquep201@gmail.com>
2021-07-10 02:09:09 +0100

    I wrote working code for the fibonacci sequence!

--- fibWithForLoop.py
+++ fibWithForLoop.py
@@ -1,4 +1,13 @@
 #okay, rethink this logic
-for i in n(range(0,10)):
-     fibonacci(n-1) + fibonacci(n-2)
-
+# for i in n(range(0,10)):
+#      fibonacci(n-1) + fibonacci(n-2)
+'''
+     THE CODE THAT WORKED!
+'''
+i = 0
+fibonacci = 0
+while i <= 10:
+    i = i+1
+    print(f"I is: {i}")
+    fibonacci = (i - 1) + (i - 2) 
+    print(f"fibonacci for {i} is: {fibonacci} ")
